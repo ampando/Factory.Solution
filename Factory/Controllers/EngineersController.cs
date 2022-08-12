85b8e78167f9fbe22498d35fd75180737fdcd90a
Adrienne <100542795+ampando@users.noreply.github.com>
2022-08-12 11:43:06 -0700

    add details and create route and views for engineer controller.

--- Factory/Controllers/EngineersController.cs
+++ Factory/Controllers/EngineersController.cs
@@ -19,5 +19,25 @@ public ActionResult Index()
   return View(_db.Engineers.ToList());
 }
 
+public ActionResult Create()
+{
+  return View();
+}
+
+[HttpPost]
+public ActionResult Create(Engineer engineer)
+{
+  _db.Engineers.Add(engineer);
+  _db.SaveChanges();
+  return RedirectToAction("Index");
+}
+public ActionResult Details(int id)
+    {
+      var thisEngineer = _db.Engineers
+          .Include(engineer => engineer.JoinEntities)
+          .ThenInclude(join => join.Machine)
+          .FirstOrDefault(engineer => engineer.EngineerId == id);
+      return View(thisEngineer);
+    }
   }
 }
